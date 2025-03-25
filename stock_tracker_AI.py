6f1fbe15debbf019bb73662a5b175fcf12bd8ffb
SAJawalkar <40295789+saneetaj@users.noreply.github.com>
2025-03-25 08:54:48 -0500

    Update stock_tracker_AI.py

--- stock_tracker_AI.py
+++ stock_tracker_AI.py
@@ -31,7 +31,7 @@ def get_stock_data(ticker):
         return None
     else:
         return None
-st.write(get_stock_data(ticker))
+st.write(get_stock_data("AAPL"))
 
 # Function to get intraday stock data (last available data when markets are closed)
 def get_intraday_data(ticker):
